995b83e8c4bcb8605c4aec421a9d9e0d527e240c
Ben Russell <BBurksrussell@gmail.com>
2013-03-13 01:22:08 -0300

    Update pyglatin.py
    -updated comments

--- pyglatin.py
+++ pyglatin.py
@@ -11,13 +11,15 @@ if len(original) > 0 and original.isalpha():
   word = original.lower()
 	first = word[0]
 	#this if/else block determines if the first letter is a vowel or 
-	#consonant.
+	#consonant. If the first letter of the string is a vowel, then it adds 'ay' to the end.
 	if first == 'a' or first == 'e' or first == 'i' or first == 'o' or first == 'u':
 		print original + pyg
-	else:
+	else: #if the first letter of the string is a consonant, then that letter is moved to
+	      #end of the string, followed by 'ay'.
 		rest = original[1:len(original)]
 		new_word =  rest + first + pyg
 		print new_word
 	
-else:
+else: # if the input is either not long enough or not made of letters, then this else statement 
+      #prints 'empty'.
 	print 'empty'
