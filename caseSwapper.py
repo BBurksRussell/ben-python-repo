930afda0b595a791ee19278d78205118e7885c55
Ben Russell <BBurksrussell@gmail.com>
2015-06-10 17:20:47 -0400

    create case swapper from work folder

---
+++ caseSwapper.py
@@ -0,0 +1,12 @@
+#Python 2.7.3 (default, Apr 10 2012, 23:31:26) [MSC v.1500 32 bit (Intel)] on win32
+#Type "copyright", "credits" or "license()" for more information.
+#This is a script to convert text in GWD from lowercase
+#to uppercase or vice versa.
+def swap(str, n):
+#The value of str which is a few words or lines of words for which I want to
+#swap case. It is passed in, possibly through command line, and the "this"
+#method helps to make it a local var for this script.
+    str = this.str;
+    conv_string = str.swapcase();
+    return conv_string;
+
